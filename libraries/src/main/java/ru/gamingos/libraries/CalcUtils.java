8d0f76b549424ee72fccd2d95eaa0bc1c7c0b6de
FluffyDaddy <sgtryan450@gmail.com>
2021-06-04 21:46:21 +0300

    Commit

--- libraries/src/main/java/ru/gamingos/libraries/CalcUtils.java
+++ libraries/src/main/java/ru/gamingos/libraries/CalcUtils.java
@@ -2,26 +2,20 @@ package ru.gamingos.libraries;
 
 public class CalcUtils {
 	private static double x, y;
-	
 	public CalcUtils () {}
-	
     public CalcUtils (final double x, final double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
 	public static final double plus (final double x, final double y) {
 		return x + y;
 	}
-	
 	public static final double mi (final double x, final double y) {
 		return x - y;
 	}
-	
 	public static final double ym (final double x, final double y) {
 		return x * y;
 	}
-	
 	public static final double ra (final double x, final double y) {
 		return x / y;
 	}
